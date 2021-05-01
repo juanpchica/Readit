f73e831f095e171b095d1d6c927a31fd3a5fe164
juanpchica <juanpchica@hotmail.com>
2021-05-01 14:51:56 -0500

    Endpoint for createPost finished and migration for users created

--- routes/posts.ts
+++ routes/posts.ts
@@ -1,17 +1,25 @@
 import {Request,Response, Router} from 'express'
+import { Post } from '../src/entities/Post';
 import auth from '../src/middlewere/auth';
 
-const createPost = (req:Request, res:Response) => {
+const createPost = async (req:Request, res:Response) => {
 
-    const {title,body,subName} = req.body;
+    const {title,body,sub} = req.body;
 
     if(title.trim() === "") return res.status(400).json({title:"Title must not be empty"});
     
+    // Get the user from auth
+    const user = res.locals.user;
+
     try{
-        // Get the user from auth
-        const user = res.locals.user;
 
+        //TODO: find sub
+        
+        //Create new post with user
+        const post = new Post({title,body,user,subName:sub});
+        await post.save();
 
+        return res.json(post);
     }catch(err){
         console.log(err);
         return res.status(500).json({ error: 'Something went wrong' })
