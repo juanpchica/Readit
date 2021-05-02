1ad83c4b1818202e7873a5402a54f4b535aa3ef4
juanpchica <juanpchica@hotmail.com>
2021-05-02 10:49:32 -0500

    Comment route added for create new comments in a post

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,4 +1,5 @@
 import {Request,Response, Router} from 'express'
+import Comment from '../entities/Comment';
 import Post from '../entities/Post';
 import Sub from '../entities/Sub';
 import auth from '../middlewere/auth';
@@ -55,11 +56,35 @@ const getPost = async (req:Request,res:Response) => {
     }
 }
 
+const commentOnPost = async (req:Request,res:Response) => {
+    const {identifier,slug} = req.params;
+    const body = req.body.body;
+    const user = res.locals.user;
+
+    if(!body || body === '') return res.status(401).json({error:"Body must not be an empty value"});
+    try {
+
+        //Find post first
+        const post = await Post.findOneOrFail({identifier,slug});
+        const comment = new Comment({body,user,post});
+
+        await comment.save();
+
+        return res.json(comment);
+
+    } catch (error) {
+        console.log(error)
+        return res.status(404).json({ error: 'Post not found' })
+    }
+
+}
+
 
 const router = Router();
 
 router.post("/",auth,createPost);
 router.get("/",getPosts);
 router.get("/:identifier/:slug",getPost);
+router.post("/:identifier/:slug/comments",auth,commentOnPost);
 
 export default router;
