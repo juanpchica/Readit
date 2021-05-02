2ca410af5ccdbe1f7dd4c89550d2649632b19e79
juanpchica <juanpchica@hotmail.com>
2021-05-02 15:45:56 -0500

    Validate errors added in server side

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,40 +1,46 @@
 import { IsEmail, Length } from "class-validator";
-import {Entity as TOEntity, Column, Index, BeforeInsert, OneToMany} from "typeorm";
+import {
+  Entity as TOEntity,
+  Column,
+  Index,
+  BeforeInsert,
+  OneToMany,
+} from "typeorm";
 
 import bcrypt from "bcrypt";
-import {Exclude} from "class-transformer"
+import { Exclude } from "class-transformer";
 
-import Entity from "./Entity"
+import Entity from "./Entity";
 import Post from "./Post";
 
 @TOEntity("users")
 export default class User extends Entity {
+  constructor(user: Partial<User>) {
+    super();
+    Object.assign(this, user);
+  }
 
-    constructor(user: Partial<User>){
-        super();
-        Object.assign(this,user);
-    }
+  @Index()
+  @IsEmail(undefined, { message: "Must be a valid email address" })
+  @Length(1, 255, { message: "Email is empty" })
+  @Column({ unique: true })
+  email: string;
 
-    @IsEmail()
-    @Index()
-    @Column({unique:true})
-    email: string
+  @Index()
+  @Length(3, 255, { message: "Must be at least 3 characters long" })
+  @Column({ unique: true })
+  username: string;
 
-    @Length(3,255,{message: "Username must be atleast 3 characteres long"})
-    @Index()
-    @Column({unique:true})
-    username: string
+  @Exclude()
+  @Column()
+  @Length(6, 255, { message: "Must be at least 6 characters long" })
+  password: string;
 
-    @Exclude()
-    @Length(6,255)
-    @Column()
-    password: string
+  @OneToMany(() => Post, (post) => post.user)
+  posts: Post[];
 
-    @OneToMany(() => Post, post => post.user)
-    posts: Post[];
-
-    @BeforeInsert()
-    async hashPassword(){
-        this.password = await bcrypt.hash(this.password,6);
-    }
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 6);
+  }
 }
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,114 +1,123 @@
+import { isEmpty, validate } from "class-validator";
+import { Request, Response, Router } from "express";
+import User from "../entities/User";
 
-import { isEmpty,validate } from 'class-validator';
-import {Request,Response,Router} from 'express'
-import User from '../entities/User';
-
-import bcrypt from 'bcrypt';
-import  jwt  from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import cookie from "cookie";
-import auth from '../middlewere/auth';
+import auth from "../middlewere/auth";
 
-const register = async (req: Request,res: Response) => {
-    const { email, username, password } = req.body;
+const mapErrors = (errors: Object[]) => {
+  return errors.reduce((prev: any, err: any) => {
+    prev[err.property] = Object.entries(err.constraints)[0][1];
+    return prev;
+  }, {});
+};
 
-    //Create a new user in db
-    try{
+const register = async (req: Request, res: Response) => {
+  const { email, username, password } = req.body;
 
-        let errors:any = {}
+  //Create a new user in db
+  try {
+    let errors: any = {};
 
-        // Validate data
-        const usernameUser = await User.findOne({username});
-        const emailUser = await User.findOne({email});
+    // Validate data
+    const usernameUser = await User.findOne({ username });
+    const emailUser = await User.findOne({ email });
 
-        if(usernameUser) errors.username = "Username is already taken";
-        if(emailUser) errors.email = "Email is already taken";
+    if (usernameUser) errors.username = "Username is already taken";
+    if (emailUser) errors.email = "Email is already taken";
 
-        if(Object.keys(errors).length > 0){
-            return res.status(400).json(errors);
-        }
-
-        // Create user
-        const user = new User({email,username,password});
-
-        //Validate error from user passed
-        errors = await validate(user);
-        if(errors.length >0) return res.status(400).json({errors});
-
-        //User Persist to DB
-        await user.save();
-
-        // Return user
-        return res.json(user);
-    }catch(err){
-        console.log(err)
-        return res.status(500).json({error:"Something went wrong"});
+    if (Object.keys(errors).length > 0) {
+      return res.status(400).json(errors);
     }
-}
 
-const login = async (req: Request, res:Response) => {
+    // Create user
+    const user = new User({ email, username, password });
 
-    const {username,password} = req.body;
-    
-    try{
-
-        let errors: any = {};
-        if(!username || username == "") errors.username = "Username must not be empty";
-        if(isEmpty(password)) errors.password = "Password must not be empty";
-
-        if(Object.keys(errors).length > 0) return res.status(400).json(errors);
-
-        const user = await User.findOne({username});
-
-        if(!user) return res.status(400).json({error:"Username not found"});
-
-        //Validate if password matches with the exist one
-        const passwordMatches = await bcrypt.compare(password,user.password);
-
-        if(!passwordMatches) return res.status(401).json({password:"Password is incorrect!"})
-
-        //Create token for user logged
-        const token = jwt.sign({username},process.env.JWT_TOKEN!);
-
-        //Set my cookie with the token created
-        res.set('Set-Cookie',cookie.serialize("token",token,{
-            httpOnly:true,
-            secure:process.env.NODE_ENV === "production",
-            sameSite:'strict',
-            maxAge:3600,
-            path:"/"
-        }))
-
-        return res.status(200).json(user);
-
-    }catch(err){
-        console.log(err);
-        return res.json({error:'Something went wrong'})
+    //Validate error from user passed
+    errors = await validate(user);
+    if (errors.length > 0) {
+      return res.status(400).json(mapErrors(errors));
     }
-}
 
-const me = async (_:Request,res:Response)=>{
-    return res.json(res.locals.user);
-}
+    //User Persist to DB
+    await user.save();
+
+    // Return user
+    return res.json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: "Something went wrong" });
+  }
+};
+
+const login = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  try {
+    let errors: any = {};
+    if (!username || username == "")
+      errors.username = "Username must not be empty";
+    if (isEmpty(password)) errors.password = "Password must not be empty";
+
+    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+    const user = await User.findOne({ username });
+
+    if (!user) return res.status(400).json({ error: "Username not found" });
+
+    //Validate if password matches with the exist one
+    const passwordMatches = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatches)
+      return res.status(401).json({ password: "Password is incorrect!" });
+
+    //Create token for user logged
+    const token = jwt.sign({ username }, process.env.JWT_TOKEN!);
+
+    //Set my cookie with the token created
+    res.set(
+      "Set-Cookie",
+      cookie.serialize("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 3600,
+        path: "/",
+      })
+    );
+
+    return res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+    return res.json({ error: "Something went wrong" });
+  }
+};
+
+const me = async (_: Request, res: Response) => {
+  return res.json(res.locals.user);
+};
 
 const logout = (_: Request, res: Response) => {
   res.set(
-    'Set-Cookie',
-    cookie.serialize('token', '', {
+    "Set-Cookie",
+    cookie.serialize("token", "", {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
       expires: new Date(0),
-      path: '/',
+      path: "/",
     })
-  )
+  );
 
-  return res.status(200).json({ success: true })
-}
+  return res.status(200).json({ success: true });
+};
 
-const router = Router()
-router.post("/register",register);
-router.post("/login",login);
-router.get("/me",auth,me);
-router.get('/logout',auth, logout)
+const router = Router();
+router.post("/register", register);
+router.post("/login", login);
+router.get("/me", auth, me);
+router.get("/logout", auth, logout);
 
 export default router;
