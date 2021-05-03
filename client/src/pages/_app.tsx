69fa4883cd262ef7a02ca856817ca0c807c1747b
juanpchica <juanpchica@hotmail.com>
2021-05-03 14:19:23 -0500

    Navbar component added with logo and search

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,11 +1,25 @@
 import Axios from "axios";
-import "../styles/globals.css";
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+
+import Navbar from "../components/Navbar";
+
+import "../styles/tailwind.css";
 
 Axios.defaults.baseURL = "http://localhost:5000/api";
 Axios.defaults.withCredentials = true;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const { pathname } = useRouter();
+  const authRoutes = ["/register", "/login"];
+  const authRoute = authRoutes.includes(pathname);
+
+  return (
+    <Fragment>
+      {!authRoute && <Navbar />}
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
 export default MyApp;
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,20 +1,11 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
-
-import logo from "../../public/images/logo.svg";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Home</title>
       </Head>
-
-      <div className='fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12'>
-        {/** Logo and title */}
-        {/** Search  */}
-        {/** Auth buttons */}
-      </div>
     </div>
   );
 }
