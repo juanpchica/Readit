17412a5bd0857d707f1554482221998d787d7617
juanpchica <juanpchica@hotmail.com>
2021-05-03 16:19:45 -0500

    Fetch post with axios and useEffect, type interface for Post added

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,11 +1,27 @@
+import axios from "axios";
 import Head from "next/head";
+import { useEffect, useState } from "react";
+
+import Post from "../types";
 
 export default function Home() {
+  const [posts, setPots] = useState<Post[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("/posts")
+      .then((res) => {
+        setPots(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
       <Head>
         <title>Home</title>
       </Head>
+      <div className='container'></div>
     </div>
   );
 }
---
+++ client/src/types.ts
@@ -0,0 +1,9 @@
+export interface Post {
+  username: string;
+  body: string;
+  createdAt: string;
+  updatedAt: string;
+  slug: string;
+  subName: string;
+  identifier: string;
+}
