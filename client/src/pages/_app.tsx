d8b75ed1a9f0122b62b446c6ef404ff4c98199d9
juanpchica <juanpchica@hotmail.com>
2021-05-03 10:18:49 -0500

    Login page component created

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import Axios from "axios";
 import "../styles/globals.css";
 
 Axios.defaults.baseURL = "http://localhost:5000/api";
+Axios.defaults.withCredentials = true;
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
