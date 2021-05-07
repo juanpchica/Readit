f133f9c9d5f75340d4ad861600f0a0828b8cbd20
juanpchica <juanpchica@hotmail.com>
2021-05-07 09:15:02 -0500

    Image type validation and delete image in case already exists

--- src/routes/subs.ts
+++ src/routes/subs.ts
@@ -3,7 +3,7 @@ import { isEmpty } from "class-validator";
 import { getRepository } from "typeorm";
 import multer, { FileFilterCallback } from "multer";
 import path from "path";
-
+import fs from "fs";
 import User from "../entities/User";
 import Sub from "../entities/Sub";
 import auth from "../middlewere/auth";
@@ -115,8 +115,36 @@ const upload = multer({
   },
 });
 
-const uploadSubImage = async (_: Request, res: Response) => {
-  return res.json({ success: true });
+const uploadSubImage = async (req: Request, res: Response) => {
+  const sub: Sub = res.locals.sub;
+  try {
+    const type = req.body.type;
+    console.log(req.file);
+
+    if (type !== "image" && type !== "banner") {
+      fs.unlinkSync(req.file.path);
+      return res.status(400).json({ error: "Invalid type" });
+    }
+
+    let oldImageUrn: string = "";
+    if (type === "image") {
+      oldImageUrn = sub.imageUrn || "";
+      sub.imageUrn = req.file.filename;
+    } else if (type === "banner") {
+      oldImageUrn = sub.bannerUrn || "";
+      sub.bannerUrn = req.file.filename;
+    }
+    await sub.save();
+
+    if (oldImageUrn !== "") {
+      fs.unlinkSync(`public\\images\\${oldImageUrn}`);
+    }
+
+    return res.json(sub);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: "Something went wrong" });
+  }
 };
 
 const router = Router();
