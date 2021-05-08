8f4bff92ba848a40b2da1b85f7b18da43d4d3960
juanpchica <juanpchica@hotmail.com>
2021-05-08 13:25:16 -0500

    User router added to fetch all data from a specific user

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,7 @@ import authRoutes from "./routes/auth";
 import postRoutes from "./routes/posts";
 import subRoutes from "./routes/subs";
 import miscRoutes from "./routes/misc";
+import usersRoutes from "./routes/users";
 
 import trim from "./middlewere/trim";
 
@@ -35,6 +36,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/subs", subRoutes);
 app.use("/api/misc", miscRoutes);
+app.use("/api/users", usersRoutes);
 
 app.listen(PORT, async () => {
   console.log(`Server is running at port ${PORT}`);
