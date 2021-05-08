022415bbe407740ba53b1fa34c69945cb06c8ca6
juanpchica <juanpchica@hotmail.com>
2021-05-08 09:35:57 -0500

    404 page added

---
+++ client/src/pages/404.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className='flex flex-col items-center'>
+      <h1 className='mt-10 mb-4 text-5xl text-gray-800'>Page Not Found</h1>
+      <Link href='/'>
+        <a className='px-4 py-2 blue button'>Home</a>
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
